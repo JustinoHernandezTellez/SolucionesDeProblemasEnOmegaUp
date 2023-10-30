84698e4dc49041fc20102dc5f6961030aaf9602b
JustinoHernandezTellez <149357560+JustinoHernandezTellez@users.noreply.github.com>
2023-10-30 00:24:29 -0600

    Add files via upload

---
+++ NivelBasico/copila y ejecutalo .cpp
@@ -0,0 +1,71 @@
+///Creado por: Justino -_- :v
+
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+void dibujar_corazon();
+
+int main ()
+{
+
+    dibujar_corazon();
+    return 0;
+}
+
+void dibujar_corazon()
+{
+    for (int i = 1; i <= 2100000000; ++i)
+        {
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n*    *    *\n";
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n*         *\n";
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n *       *\n";
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n  *     *\n";
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n   *   *\n";
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n     *   *\n    * *\n";
+
+
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n     *   *\n      * *\n     *";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n     *   *\n      * *\n       *";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n     *   *\n      * *\n     *";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n     *   *\n    * *\n";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n    *     *\n   *   *\n";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n  *         *\n   *       *\n  *     *\n";
+                   usleep(90000);
+                   system("cls");
+                   cout << "   **** ****\n  *    *    *\n*         *\n";
+                   usleep(90000);
+                   system("cls");
+
+
+        }
+}
